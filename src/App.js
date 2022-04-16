0a45dbe65b74d6e5df5deb6fe9d6636283e88625
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-04-17 00:15:11 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Footer from './pages/Footer/Footer';
+import Header from './pages/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header></Header>
+      <Footer></Footer>
 
     </div>
   );
--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">TWB</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Home</Nav.Link>
+                        <Nav.Link href="#pricing">Packages</Nav.Link>
+                        <Nav.Link href="#pricing">Blogs</Nav.Link>
+                        <Nav.Link href="#pricing">Contact</Nav.Link>
+                    </Nav>
 
-        </div>
+                </Navbar.Collapse>
+                <Form className="d-flex">
+                    <Button variant="outline-success mx-2">Login</Button>
+                    <Button variant="outline-success">SignUp</Button>
+                </Form>
+            </Container>
+        </Navbar>
     );
 };
 
