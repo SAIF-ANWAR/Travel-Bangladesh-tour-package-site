16481358f016d67756ca2434adfaf08099fb417a
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-04-18 12:56:46 +0600

    blogs

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -13,10 +13,11 @@ const Blogs = () => {
             </div>
             <div>
                 <h1>Why do I use Firebase?  What other options do you have to implement authentication?</h1>
-                <p>Ans: Authorization is the process of controlling access to some of the functions in an application.
-                    It allows one party to access other resources.
-                    Whereas authentication is the process of checking the users' identity to access functions. it may request user name, password or any kind
-                    of input required to validate the user identity.
+                <p>Ans: Firebase provides me server-less backend for my mobile and web applications.
+                    I do not reuire to develop my own backend. It saves a lot of time and cost to build my own backend.
+                    Firebase also provides secured authentication system.
+                    <br />
+
                 </p>
             </div>
         </div>
